ab14ae92619ad5e893eb1cac0e0203daaf15800f
Hades1412 <209030wolf555@gmail.com>
2024-02-05 21:19:24 -0500

    Added mouse tracking
    Only the camera position is moving, not the basket position oddly

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class ApplePicker : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i=0; i<numBaskets; i++) 
+            {
+            GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + ( basketSpacingY * i);
+            tBasketGO.transform.position = pos;
+            }
     }
-
     // Update is called once per frame
     void Update()
     {
+        
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
         
     }
 }
