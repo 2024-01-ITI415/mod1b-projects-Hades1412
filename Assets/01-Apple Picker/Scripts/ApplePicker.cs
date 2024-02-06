b948c01cbfed729437ab6627083d2339f337c59a
Hades1412 <209030wolf555@gmail.com>
2024-02-05 23:00:57 -0500

    Added HighScore Tracking

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -37,6 +38,10 @@ public class ApplePicker : MonoBehaviour
         GameObject tBasketGO = basketList[basketIndex];
         basketList.RemoveAt(basketIndex);
         Destroy(tBasketGO);
+        if (basketList.Count == 0)
+        {
+            SceneManager.LoadScene("Main-ApplePicker");
+        }
     }
     // Update is called once per frame
     void Update()
