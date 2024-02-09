34993b7dfde2a0caf6348a1f41619c9988a9e76b
Hades1412 <209030wolf555@gmail.com>
2024-02-09 09:46:26 -0500

    Finished Slingshot Script, started FollowCam

---
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;
+    [Header("Set Dynamically")]
+    public float camZ;
+
+    void Awake()
+    {
+        camZ=this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        if (POI == null) return;
+
+        Vector3 destination = POI.transform.position;
+        destination.z = camZ;
+        transform.position = destination;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
